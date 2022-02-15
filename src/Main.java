a4a5ace7f3d0e35ffb8ab6f38b2c0691dedd4445
Alex-Oq21 <omaroquendo221@gmail.com>
2022-02-15 16:26:36 -0500

    Second step is done

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.sql.Driver;
+
 public class Main {
     public static void main(String[] args) {
-        //System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        try {
+       /* try{
             driver.get("https://www.youtube.com/");
 
             Thread.sleep(5000);
-            
             String title = driver.getTitle();
             System.out.println(title);
-        } catch (InterruptedException e) {
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            driver.quit();
+        }*/
+
+        try{
+           driver.get("https://es.wikipedia.org/wiki/Wikipedia:Portada");
+           Thread.sleep(2000);
+           driver.findElement(By.id("n-randompage")).click();
+           Thread.sleep(2000);
+           String title = driver.getTitle();
+            System.out.println(title);
+        }catch (InterruptedException e){
             e.printStackTrace();
         }finally {
             driver.quit();
         }
-
-
     }
 }
