bc52e4fa0b3811341df1ebd745e6dbc30a6bd172
Alex-Oq21 <omaroquendo221@gmail.com>
2022-02-16 22:05:59 -0500

    Practice with Explicit wait is done.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.*;
 
 import java.io.IOException;
 import java.sql.Driver;
@@ -26,24 +25,39 @@ public class Main {
             e.printStackTrace();
         }finally {
             driver.quit();
-        }*/
+        }
+        //--------------------------------------------------------------------
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         try{
            driver.get("http://127.0.0.1:5500/index_completo.html");
 
            //driver.findElement(By.id("implicitWaitButton")).click();
+          Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                  .withTimeout(Duration.ofSeconds(10))
+                  .pollingEvery(Duration.ofSeconds(3))
+                  .ignoring(NoSuchElementException.class);
+          WebElement webElement = wait.until(new Function<WebDriver, WebElement>() {
+              @Override
+              public WebElement apply(WebDriver webDriver) {
+                  return driver.findElement(By.id("implicitWaitButton"));
+              }
+          });
+            webElement.click();
+            Thread.sleep(4000);
 
-            Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-                    .withTimeout(Duration.ofSeconds(10))
-                    .pollingEvery(Duration.ofSeconds(2))
-                    .ignoring(NoSuchElementException.class);
-            WebElement webElement = wait.until(new Function<WebDriver, WebElement>() {
-                @Override
-                public WebElement apply(WebDriver driver) {
-                    return driver.findElement(By.id("implicitWaitButton"));
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            driver.quit();
+        }
 
-                }
-            });
+        */
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        try{
+            driver.get("http://127.0.0.1:5500/index_completo.html");
+
+            //WebDriverWait driverWait = new WebDriverWait(driver, 10);
+            WebElement webElement = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.id("explicitWaitButton")));
             webElement.click();
             Thread.sleep(4000);
 
